dd0c56229f0999736a0eb0d1dc1aea45fcbc15de
Karan Thakkar <thakkar.karan04@gmail.com>
2024-01-04 23:19:30 -0500

    Update node_app.sh

--- 2 - Operating Systems & Linux Basics/node_app.sh
+++ 2 - Operating Systems & Linux Basics/node_app.sh
@@ -6,13 +6,13 @@ SERVICE_ACCOUNT_USER=myapp
 echo "Installing Node.js..."
 sudo apt update && sudo apt upgrade -y; sudo apt install -y nodejs npm curl net-tools
 
-echo -e "\n\n"
+echo -e "\n"
 
 # Print the installed versions
 echo "NodeJS version: $(node -v)"Y
 echo "NPM version: $(npm -v)"
 
-echo -e "\n\n"
+echo -e "\n"
 
 # Get the log directory from the parameter input
 LOG_DIRECTORY=$1
@@ -28,7 +28,7 @@ else
   mkdir -p $LOG_DIRECTORY
 fi
 
-echo -e "\n\n"
+echo -e "\n"
 
 # Create myapp user 
 echo "Creating 'myapp' user..."
@@ -36,81 +36,40 @@ sudo useradd -m -s /bin/bash $SERVICE_ACCOUNT_USER
 # disable password for this user
 sudo passwd -d $SERVICE_ACCOUNT_USER
 
-echo -e "\n\n"
+echo -e "\n"
 
 # Changing ownership of the log directory to $SERVICE_ACCOUNT_USER
 echo "Setting permissions..."
 sudo chown -R $SERVICE_ACCOUNT_USER "$LOG_DIRECTORY"
 
-echo -e "\n\n"
-
-# Switch to application user 
-echo "Switching to 'myapp' user..."
-su - myapp
-
-echo -e "\n\n"
+echo -e "\n"
 
 # Download the artifact file using curl
 echo "Downloading application code..."
-curl -L -O https://node-envvars-artifact.s3.eu-west-2.amazonaws.com/bootcamp-node-envvars-project-1.0.0.tgz
+sudo su - $SERVICE_ACCOUNT_USER -c "curl -L -O https://node-envvars-artifact.s3.eu-west-2.amazonaws.com/bootcamp-node-envvars-project-1.0.0.tgz"
 
-echo -e "\n\n"
-
-# Create a directory to store app files
-mkdir -p myproject
+echo -e "\n"
 
 # Extract and decompress the project files into the myproject directory
 echo "Extracting project files..."
-tar -xvzpf bootcamp-node-envvars-project-1.0.0.tgz -C ~/myproject
+sudo su - $SERVICE_ACCOUNT_USER -c "tar -xvzf ./bootcamp-node-envvars-project-1.0.0.tgz"
 
-echo -e "\n\n"
+echo -e "\n"
 
-# Set the environment variables
-export APP_ENV=dev
-export DB_USER=myuser
-export DB_PWD=mysecret
+# Set the environment variables and change into the unzipped package directory
+sudo su - $SERVICE_ACCOUNT_USER -c "
+  export APP_ENV=dev &&
+  export DB_USER=myuser &&
+  export DB_PWD=mysecret &&
+  export LOG_DIR=$(realpath "$LOG_DIRECTORY") &&
+  cd package && 
+  npm install && 
+  node server.js &"
 
-# Set the LOG_DIR environment variable to the absolute path of the log directory
-export LOG_DIR=$(realpath "$LOG_DIRECTORY")
+echo -e "\n"
 
-echo -e "\n\n"
-
-# Change into the unzipped package directory
-cd myproject
-
-echo -e "\n\n"
-
-# Install dependencies
-echo "Installing node modules..."
-npm install
-
-echo -e "\n\n"
-
-# Start application in background 
-echo "Starting application..."
-node server.js &
-
-echo -e "\n\n"
-
-# Get the process ID of the NodeJS application
-pid=$!
-
-# Wait for a few seconds to let the application start
-echo "Waiting for application to start..."
-sleep 10
-
-# Check if the process is still running
-if ps -p $pid > /dev/null; then
-  # Process is running
-  echo "Node app is running with PID: $pid"
-  # Get the port number from the server.js file
-  port=$(grep -oP 'app.listen\(\K\d+' server.js)
-  # Print the port number
-  echo "Node app is listening on port: $port"
-else
-  # Process is not running
-  echo "Node app is not running. Please check the logs for errors."
-fi
-
-# Log out from myapp user
-exit
+# Verify app is running as myapp user  
+sudo su - myapp -c "
+  ps aux | grep node
+  netstat -tunlp | grep LISTEN
+"
