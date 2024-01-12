373d4c8fe4e6e5201ae7e271edb7c9fbd47b3854
Karan Thakkar <thakkar.karan04@gmail.com>
2024-01-11 20:30:13 -0500

    Add parameter handling

--- 4 - Build Tools & Package Manager Tools/build-tools-exercises/src/main/java/com/example/Application.java
+++ 4 - Build Tools & Package Manager Tools/build-tools-exercises/src/main/java/com/example/Application.java
@@ -13,7 +13,17 @@ public class Application {
     public static void main(String[] args)
     {
         SpringApplication.run(Application.class, args);
-        // add your code snippet here
+        // Code snippet for Exercise 6!
+        // Code snippet to add inside Application.java on line 16
+
+        Logger log = LoggerFactory.getLogger(Application.class);
+        try {
+            String one = args[0];
+            String two = args[1];
+            log.info("Application will start with the parameters {} and {}", one, two);
+        } catch(Exception e) {
+            log.info("No parameters provided");
+        }
     }
 
     @PostConstruct
