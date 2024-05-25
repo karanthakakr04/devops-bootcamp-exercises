a32661d65060776c0a1bcdb6efbefaf23eca6e62
Karan Thakkar <thakkar.karan04@gmail.com>
2024-05-25 10:09:36 -0400

    Update server.js

--- 9 - AWS Services/aws-exercises/app/server.js
+++ 9 - AWS Services/aws-exercises/app/server.js
@@ -1,26 +1,26 @@
-let express = require('express');
-let path = require('path');
-let fs = require('fs');
-let app = express();
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+const app = express();
 
-
-app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, "index.html"));
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.get('/profile-picture-andrea', function (req, res) {
-  let img = fs.readFileSync(path.join(__dirname, "images/profile-andrea.jpg"));
-  res.writeHead(200, {'Content-Type': 'image/jpg' });
-  res.end(img, 'binary');
+app.get('/profile-picture-andrea', (req, res) => {
+    const img = fs.readFileSync(path.join(__dirname, 'images/profile-andrea.jpg'));
+    res.writeHead(200, { 'Content-Type': 'image/jpeg' });
+    res.end(img, 'binary');
 });
 
-app.get('/profile-picture-ari', function (req, res) {
-  let img = fs.readFileSync(path.join(__dirname, "images/profile-ari.jpeg"));
-  res.writeHead(200, {'Content-Type': 'image/jpg' });
-  res.end(img, 'binary');
+app.get('/profile-picture-ari', (req, res) => {
+    const img = fs.readFileSync(path.join(__dirname, 'images/profile-ari.jpeg'));
+    res.writeHead(200, { 'Content-Type': 'image/jpeg' });
+    res.end(img, 'binary');
 });
 
-app.listen(3000, function () {
-  console.log("app listening on port 3000!");
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}!`);
 });
 
