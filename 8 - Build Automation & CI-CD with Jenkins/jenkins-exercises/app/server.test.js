098eb83133926b8d8a3a2f54169cb7a4ace89065
Karan Thakkar <thakkar.karan04@gmail.com>
2024-06-03 22:23:12 -0400

    Update server.test.js

--- 8 - Build Automation & CI-CD with Jenkins/jenkins-exercises/app/server.test.js
+++ 8 - Build Automation & CI-CD with Jenkins/jenkins-exercises/app/server.test.js
@@ -1,7 +1,15 @@
 const path = require('path');
 const fs = require('fs');
 const request = require('supertest');
-const app = require('./server');
+let server;
+
+beforeAll(() => {
+  server = require('./server'); // Import the server instance
+});
+
+afterAll((done) => {
+  server.close(done); // Close the server after tests are completed
+});
 
 test('main index.html file exists', () => {
   const filePath = path.join(__dirname, 'index.html');
@@ -9,19 +17,19 @@ test('main index.html file exists', () => {
 });
 
 test('should serve the index.html file', async () => {
-  const response = await request(app).get('/');
+  const response = await request(server).get('/');
   expect(response.status).toBe(200);
   expect(response.text).toContain('<h1>List of projects team is working on</h1>');
 });
 
 test('should serve the profile image for Andrea', async () => {
-  const response = await request(app).get('/profile-picture-andrea');
+  const response = await request(server).get('/profile-picture-andrea');
   expect(response.status).toBe(200);
   expect(response.headers['content-type']).toContain('image/jpg');
 });
 
 test('should serve the profile image for Ari', async () => {
-  const response = await request(app).get('/profile-picture-ari');
+  const response = await request(server).get('/profile-picture-ari');
   expect(response.status).toBe(200);
   expect(response.headers['content-type']).toContain('image/jpg');
 });
