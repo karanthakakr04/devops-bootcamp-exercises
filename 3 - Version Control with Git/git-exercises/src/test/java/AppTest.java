59767ea8f27a880755c59bd571d25d1b29b0faae
Karan Thakkar <thakkar.karan04@gmail.com>
2024-08-16 11:55:11 -0400

    Update AppTest.java

--- 3 - Version Control with Git/git-exercises/src/test/java/AppTest.java
+++ 3 - Version Control with Git/git-exercises/src/test/java/AppTest.java
@@ -1,7 +1,8 @@
-import com.example.Application;
-import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import static org.junit.Assert.assertEquals;
+import org.junit.jupiter.api.Test;
+
+import com.example.Application;
 
 public class AppTest {
 
